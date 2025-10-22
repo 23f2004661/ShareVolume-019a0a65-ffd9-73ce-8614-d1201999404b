a248ad0c34f49a09973c22d97257189035a4601b
23f2004661 <23f2004661@ds.study.iitm.ac.in>
2025-10-22 11:01:04 +0530

    Add script.js - round 1

---
+++ script.js
@@ -0,0 +1,3 @@
+// The JavaScript logic is embedded directly within index.html to fulfill the requirement of a frontend-only app.
+// This file serves as a placeholder to acknowledge the structure if a separate JS file were preferred,
+// but for this specific task, all functionality resides in index.html.
